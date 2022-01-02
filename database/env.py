f3bae60ff1883677a83416540f623e4e7bc3052a
Omar Karazoun <omar.kar101@gmail.com>
2022-01-02 17:45:19 +0200

    doing first migrations

--- database/env.py
+++ database/env.py
@@ -1,8 +1,7 @@
 from logging.config import fileConfig
-
+from orm import Base
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
 from alembic import context
 
 # this is the Alembic Config object, which provides
@@ -17,7 +16,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
---
+++ database/orm.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('postgresql://postgres:test-pass-123@localhost:3306/my-app')
+Base = declarative_base()
+
+Session = sessionmaker(bind=engine)
+
+def generate_db_session():
+    return Session()
