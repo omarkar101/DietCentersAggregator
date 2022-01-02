d95e45e68ccdc777e18119abb6388eb423e994e8
Omar Karazoun <omar.kar101@gmail.com>
2022-01-02 18:50:29 +0200

    init flaskk

--- database/orm.py
+++ database/orm.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('postgresql://postgres:test-pass-123@localhost:3306/my-app')
+engine = create_engine('postgresql://postgres:test-pass-123@localhost:3306/299')
 Base = declarative_base()
 
 Session = sessionmaker(bind=engine)
