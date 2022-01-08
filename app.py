b39275f05ba959679986e391316a18642c8eb941
Omar Karazoun <omar.kar101@gmail.com>
2022-01-08 17:13:46 +0200

    added api

--- app.py
+++ app.py
@@ -6,15 +6,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return '/'
 
-@app.route('/addUser')
-def add_user():
-    db_session = generate_db_session()
-    user = User()
-    db_session.add(user)
-    db_session.commit()
-    return f'added user {user.id}'
+@app.route('/home')
+def home():
+    return 'home'
 
 if __name__ == '__main__':
     app.run()
