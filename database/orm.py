70b662252ba13a5124fea02235db81eb5c266971
Omar Karazoun <omar.kar101@gmail.com>
2022-01-08 18:11:21 +0200

    ....

--- database/orm.py
+++ database/orm.py
@@ -2,7 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# !!!!!!!!!!!!!!!!!!!!!!!!for deployment environment only!!!!!!!!!!!!!!!!!!!!!!!
 engine = create_engine('postgresql://bepclhck:wuNRDdZ4XGYDBCjIZgw6nAL2aUh-9OOx@abul.db.elephantsql.com/bepclhck')
+
+# !!!!!!!!!!!!!!!!!!!!!!!!for development environment only!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# enginer = create_engine('postgresql://postgres:test-pass-123@localhost:3306/299')
+
 Base = declarative_base()
 
 Session = sessionmaker(bind=engine)
