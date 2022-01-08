4d45c16f52f59f8229bdd69ebb66bdf7cce0edf0
Omar Karazoun <omar.kar101@gmail.com>
2022-01-08 16:25:50 +0200

    init db

--- database/models/users.py
+++ database/models/users.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Text, BigInteger
+from sqlalchemy import Column, BigInteger
 from database.orm import Base
 
 metadata = Base.metadata
@@ -7,4 +7,3 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(BigInteger, primary_key=True, autoincrement=True)
-    first_name = Column(Text)
