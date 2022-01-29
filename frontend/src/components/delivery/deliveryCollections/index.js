d9b8f740cb7891d1b64bdd0422cc3cb33b00530f
Ali <73133529+SrourHub@users.noreply.github.com>
2022-01-29 20:46:04 +0200

    delivery filters and css 1 hr 49 mins in

---
+++ frontend/src/components/delivery/deliveryCollections/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./deliveryCollections.css";
+
+const DeliveryCollections = () => {
+    return (
+        <div className="deliver-collection">
+            <div className="max-width">
+                <div className="collection-title">Eat what makes you happy</div>
+            </div>
+        </div>
+    );
+};
+
+export default DeliveryCollections;
