1b52ee9aed61a520decdb98abf4a940b6357c881
Omar Karazoun <omar.kar101@gmail.com>
2022-01-29 17:39:42 +0200

    deleted unneeded api

--- app.py
+++ app.py
@@ -4,9 +4,5 @@ from auth.api.login import login_api
 app = Flask(__name__)
 app.register_blueprint(login_api, url_prefix='/login')
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
-
 if __name__ == '__main__':
     app.run(debug=True)
