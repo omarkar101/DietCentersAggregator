cd9a054acb3d9ba4a54c6dd873f3a5d922ab2709
Ali <73133529+SrourHub@users.noreply.github.com>
2022-01-29 18:36:20 +0200

    first 30 minutes of the youtube

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,8 @@
+import React from 'react'
+import HomePage from './pages/home';
 
 const App = () => {
-  return (
-    <>
-      Hello
-    </>
-  );
+  return <HomePage />;
 }
 
 export default App;
---
+++ frontend/src/components/common/tabOptions/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TabOptions = ({ activeTab, setActiveTab }) => {
+    return <div>
+        this is khara Tab Options
+    </div>;
+}
+
+export default TabOptions;
