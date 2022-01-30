32a03c8ea1e00fa9eb00ea8b5ce4693f68950cd8
Ali <73133529+SrourHub@users.noreply.github.com>
2022-01-30 14:57:25 +0200

    finished delivery page 3hrs 8 mins in

--- frontend/src/components/common/footer/index.js
+++ frontend/src/components/common/footer/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
     return <div>
-        This is footer
+        Ali Srour is King
     </div>;
 }
 
---
+++ frontend/src/components/delivery/topBrands/index.js
@@ -0,0 +1,67 @@
+import React from "react";
+import Slider from "react-slick";
+import NextArrow from "../../common/carousel/nextArrow";
+import PrevArrow from "../../common/carousel/prevArrow";
+import "./topBrands.css";
+
+const topBrandsList = [
+    {
+        id: 1,
+        time: "35 min",
+        cover:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/1200px-McDonald%27s_Golden_Arches.svg.png"
+    },
+    {
+        id: 2,
+        time: "29 min",
+        cover:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/1200px-McDonald%27s_Golden_Arches.svg.png"
+    },
+    {
+        id: 3,
+        time: "32 min",
+        cover:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/1200px-McDonald%27s_Golden_Arches.svg.png"
+    },
+    {
+        id: 4,
+        time: "30 min",
+        cover:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/1200px-McDonald%27s_Golden_Arches.svg.png"
+    },
+    {
+        id: 5,
+        time: "25 min",
+        cover:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/1200px-McDonald%27s_Golden_Arches.svg.png"
+    },
+];
+
+const settings = {
+    infinite: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />
+}
+
+const TopBrands = () => {
+    return (
+        <div className="top-brands max-width">
+            <div className="collection-title">Top Brands for you</div>
+            <Slider {...settings}>
+                {topBrandsList.map((brand) => {
+                    return (
+                        <div>
+                            <div className="top-brands-cover">
+                                <img src={brand.cover} className="top-brands-image" alt={brand.time} />
+                            </div>
+                        </div>
+                    );
+                })}
+            </Slider>
+        </div>
+    );
+}
+
+export default TopBrands;
