a629b4bf83860425b6dee7702aafee67e6f39ddb
Ali <73133529+SrourHub@users.noreply.github.com>
2022-01-30 19:09:23 +0200

    starting with the login and signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import HomePage from './pages/home';
+import LoginForm from './pages/login';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
-  return <HomePage />;
+  return <LoginForm />;
 }
 
 export default App;
