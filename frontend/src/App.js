94687d5c284cc2552d6af27bd30918c38917cb90
Ali <73133529+SrourHub@users.noreply.github.com>
2022-01-31 22:03:58 +0200

    routing template to use later in addition to navbar if needed....

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,9 +5,46 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import SignUp from './pages/signup';
 import TrackOrder from './pages/ordersPage/trackOrder';
 import Collection from './components/common/collection';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
 
 const App = () => {
-  return <HomePage />;
+  return (
+    <BrowserRouter>
+    <>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand as={Link} to="/">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">Features</Nav.Link>
+              <Nav.Link as={Link} to="/">Pricing</Nav.Link>
+              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                <NavDropdown.Item as={Link} to="/">Action</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/">Another action</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item as={Link} to="/">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+              <Nav.Link as={Link} to="/">More deets</Nav.Link>
+              <Nav.Link eventKey={2} as={Link} to="/signup">
+                Signup
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div>
+        <Routes>
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </div>
+    </>
+    </BrowserRouter>
+  );
 }
 
 export default App;
