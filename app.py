9727b9bfc5bd91e9d3313ed7db314e3fb8ae896d
Rony_Allaw <rga45@mail.aub.edu>
2022-02-02 20:36:08 +0200

    Made initial steps in JWT authentication

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from auth.api import auth_api
+from auth.decorators import token_required
 
 from database.models.credentials import Credentials
 from database.models.addresses import Address
@@ -11,7 +12,12 @@ from database.models.service_providers_meal_plans import ServiceProviderMealPlan
 from database.models.meal_plans_prices import MealPlanPrice
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'this_key_here'
 app.register_blueprint(auth_api)
 
+@app.route('/',methods = ['POST'])
+@token_required
+def hello():
+    return 'hello'
 if __name__ == '__main__':
     app.run(debug=True)
--- auth/decorators.py
+++ auth/decorators.py
@@ -1,6 +1,8 @@
 from functools import wraps
-from flask import abort
-
+from flask import abort, request, jsonify
+from database.models.users import User
+from flask import current_app
+import jwt
 def require_user(f):
     @wraps(f)
     def wrap(*args, **kwargs):
@@ -9,3 +11,29 @@ def require_user(f):
         else:
             abort(401)
     return wrap
+    """
+    This piece of code is taken from:
+    https://www.geeksforgeeks.org/using-jwt-for-user-authentication-in-flask/
+    """
+# decorator for verifying the JWT
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        # jwt is passed in the request header
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        # return 401 if token is not passed
+        if not token:
+            return jsonify({'message' : 'Token is missing !!'}), 401
+        try:
+            # decoding the payload to fetch the stored details
+            a = jwt.decode(token,current_app.config['SECRET_KEY'],algorithms=['HS256'])
+        except:
+            return jsonify({
+                'message' : 'Token is invalid !!'
+            }), 401
+        # returns the current logged in users contex to the routes
+        return  f('Success', *args, **kwargs)
+  
+    return decorated
