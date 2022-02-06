d92d6d65e3c64e7e5a3644b65a5ad9fcb1a08164
Ali <73133529+SrourHub@users.noreply.github.com>
2022-02-06 20:47:02 +0200

    f

--- frontend/src/api/requests.js
+++ frontend/src/api/requests.js
@@ -1,1 +1,11 @@
 import { Get, Post } from "./setup";
+
+export const signUpClient = (email, password, firstName, lastName, phoneNumber) => {
+  const data = new FormData();
+  data.append('first_name', firstName);
+  data.append('last_name', lastName);
+  data.append('phone_number', phoneNumber);
+  data.append('email', email);
+  data.append('password', password);
+  return Post('/auth/signup/user', data);
+}
--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -5,7 +5,7 @@ const Login = () => {
     return (
         <>
             <Container>
-                <h1 className="shadow-sm text-success mt-5 p-3 text-center rounded">Admin Login</h1>
+                <h1 className="shadow-sm text-success mt-5 p-3 text-center rounded">Login</h1>
                 <Row className="mt-5">
                     <Col lg={5} md={6} sm={12} className="p-5 m-auto shadow-sm rounded-lg">
                         <Form>
