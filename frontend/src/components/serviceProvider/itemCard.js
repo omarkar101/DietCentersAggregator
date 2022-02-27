9f345892cdf5fd3fdc47f32eb67db9e0733aea7c
Ali <73133529+SrourHub@users.noreply.github.com>
2022-02-27 18:23:57 +0200

    done with the package page still needs some design

--- frontend/src/components/serviceProvider/itemCard.js
+++ frontend/src/components/serviceProvider/itemCard.js
@@ -16,12 +16,6 @@ const ItemCard = (props) => {
                 {item.description}
               </ItemDescriptionParagraph>
             </ItemTextWrapper>
-            <ButtonContainer>
-              <AddSpanStyle>ADD</AddSpanStyle>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="#CFCFCF" width="16" height="16" viewBox="0 0 20 20" aria-labelledby="icon-svg-title- icon-svg-desc-" role="img">
-                <path d="M15.5 9.42h-4.5v-4.5c0-0.56-0.44-1-1-1s-1 0.44-1 1v4.5h-4.5c-0.56 0-1 0.44-1 1s0.44 1 1 1h4.5v4.5c0 0.54 0.44 1 1 1s1-0.46 1-1v-4.5h4.5c0.56 0 1-0.46 1-1s-0.44-1-1-1z"></path>
-              </svg>
-            </ButtonContainer>
           </ItemTextAndButtonWrapper>
         </ItemWrapper>
     );
