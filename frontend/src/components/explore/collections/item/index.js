1bfef49671707519db0710c3bb8a7f6769a419ef
Ali <73133529+SrourHub@users.noreply.github.com>
2022-02-27 18:32:06 +0200

    Update index.js

--- frontend/src/components/explore/collections/item/index.js
+++ frontend/src/components/explore/collections/item/index.js
@@ -4,14 +4,12 @@ import "./item.css";
 
 const Item = ({ item }) => {
   return (
-    <Link to={`/serviceProvider/${1}`} style={{ color: 'inherit', textDecoration: 'inherit'}}>
       <div>
         <div className='delivery-item-cover'>
           <img src={item.cover} className='delivery-item-image' alt={item.title} />
         </div>
         <div className='delivery-item-title'>{item.title}</div>
       </div>
-    </Link>
   );
 };
 
