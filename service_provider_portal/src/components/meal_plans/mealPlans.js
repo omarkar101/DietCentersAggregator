e9e4ec354258e181abe5843034adfee5a25296ad
Ali <73133529+SrourHub@users.noreply.github.com>
2022-03-01 17:59:41 +0200

    adding new components for the meal plan page

--- service_provider_portal/src/components/meal_plans/mealPlans.js
+++ service_provider_portal/src/components/meal_plans/mealPlans.js
@@ -1,27 +1,97 @@
+import { useCallback, useReducer, useState } from "react";
 import { Button, Table } from "react-bootstrap";
 import styled from "styled-components";
+import MealPlanModal from "./meal_plan_modal";
 
-const MealPlans = () => {
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'open-add-meal-plan-modal':
+      return {modalOpen: true, selectedMealPlanId: null, selectedMealPlanDescription: null, selectedMealPlanName: null};
+    case 'submit-add-meal-plan-modal':
+      return {modalOpen: false, selectedMealPlanDescription: action.mealPlanDescription, selectedMealPlanName: action.mealPlanName};
+    case 'delete-meal-plan':
+      return {};
+    case 'open-edit-meal-plan-modal':
+      return {modalOpen: true, selectedMealPlanDescription: action.mealPlanDescription, selectedMealPlanName: action.mealPlanName};
+    case 'submit-edit-meal-plan-modal':
+      return {modalOpen: false, selectedMealPlanDescription: action.mealPlanDescription, selectedMealPlanName: action.mealPlanName,
+        selectedMealPlanId: action.mealPlanId};
+    case 'close-meal-plan-modal':
+      return {modalOpen: false, selectedMealPlanDescription: null, selectedMealPlanName: null, selectedMealPlanId: null};
+    default:
+      throw new Error();
+  }
+}
+
+const MealPlans = (props) => {
+
+  const [state, dispatch] = useReducer(reducer, {
+    modalOpen: false,
+    selectedMealPlanId: null,
+    selectedMealPlanDescription: null,
+    selectedMealPlanName: null
+  });
+
+  const mealPlans = [
+    {'name': 'Burger', 'description': 'Hello World', 'categories': 'Fast Food'},
+    {'name': 'Burger', 'description': 'Hello World', 'categories': 'Fast Food'},
+    {'name': 'Burger', 'description': 'Hello World', 'categories': 'Fast Food'}
+  ]
+
+  const toggleDeleteMealPlan = useCallback((e) => {
+    console.log('delete');
+    dispatch({type: 'delete-meal-plan'});
+  }, []);
+
+  const toggleOpenModal = useCallback((e) => {
+    const mealPlanId = e.target.id;
+    const mealPlanName = e.target.dataset.mealPlanName;
+    const mealPlanDescription = e.target.dataset.mealPlanDescription;
+    console.log(mealPlanDescription, mealPlanName);
+    dispatch({type: 'open-edit-meal-plan-modal', mealPlanDescription: mealPlanDescription, mealPlanName: mealPlanName});
+  }, []);
+
+  const toggleModalOnSubmit = useCallback(() => {
+    dispatch({type: 'close-meal-plan-modal'});
+  }, []);
+
+  const toggleModalOnClose = useCallback(() => {
+    dispatch({type: 'close-meal-plan-modal'});
+  }, []);
+
   return (
     <>
       <Container>
-      <Button variant="success">Add Package</Button>
-          <Table striped bordered hover>
+        <MealPlanModal isOpen={state.modalOpen} onClose={toggleModalOnClose} onSubmit={toggleModalOnSubmit}
+          mealPlanName={state.selectedMealPlanName} mealPlanDescription={state.selectedMealPlanDescription} />
+        <Button variant="success" onClick={toggleOpenModal}>Add Meal Plan</Button>
+        <Table striped bordered hover>
+          <tr>
+            <th>Name</th>
+            <th>Category</th>
+            <th>Description</th>
+            <th>Actions</th>
+          </tr>
+          {mealPlans.map((mealPlan, index) =>
             <tr>
-              <th>Name</th>
-              <th>Category</th>
-              <th>Description</th>
-              <th>Actions</th>
-            </tr>
-            <tr>
-              <td>Burger</td>
-              <td>Fast Food</td>
+              <td>{mealPlan.name}</td>
+              <td>{mealPlan.description}</td>
               <td>This is food</td>
-            </tr>
-          </Table>
-        </Container>
-    </>
-  );
+              <td>
+                <div className="mb-2">
+                  <Button id={index} data-mealPlanName={mealPlan.name} data-mealPlanDescription={mealPlan.description} variant="primary"
+                      size="sm" onClick={toggleOpenModal}>
+                    Edit
+                  </Button>
+                  <Button id={index} variant="danger" size="sm" onClick={toggleDeleteMealPlan}>
+                    Delete
+                  </Button>
+                </div>
+              </td>
+            </tr>)}
+        </Table>
+      </Container>
+    </>);
 }
 
 const Container = styled.div`
---
+++ service_provider_portal/src/components/meal_plans/meal_plan_modal.js
@@ -0,0 +1,132 @@
+import React from "react";
+import { useCallback, useReducer, useState } from "react";
+import { Button, Form, Modal, Table } from "react-bootstrap";
+import styled from "styled-components";
+import ItemModal from "../items/item_modal";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'open-add-item-modal':
+      return {modalOpen: true, selectedItemId: null, selectedItemDescription: null, selectedItemName: null};
+    case 'submit-add-item-modal':
+      return {modalOpen: false, selectedItemDescription: action.itemDescription, selectedItemName: action.itemName};
+    case 'delete-item':
+      return {};
+    case 'open-edit-item-modal':
+      return {modalOpen: true, selectedItemDescription: action.itemDescription, selectedItemName: action.itemName};
+    case 'submit-edit-item-modal':
+      return {modalOpen: false, selectedItemDescription: action.itemDescription, selectedItemName: action.itemName,
+        selectedItemId: action.itemId};
+    case 'close-item-modal':
+      return {modalOpen: false, selectedItemDescription: null, selectedItemName: null, selectedItemId: null};
+    default:
+      throw new Error();
+  }
+}
+
+const MealPlanModal = (props) => {
+  const { isOpen, onClose, onSubmit, mealPlanName, mealPlanDescription } = props;
+
+  const [state, dispatch] = useReducer(reducer, {
+    modalOpen: false,
+    selectedItemId: null,
+    selectedItemDescription: null,
+    selectedItemName: null
+  });
+
+  const items = [
+    { name: "Burger", description: "Hello World", categories: "Fast Food" },
+    { name: "Burger", description: "Hello World", categories: "Fast Food" },
+    { name: "Burger", description: "Hello World", categories: "Fast Food" },
+  ];
+
+
+  const toggleDeleteItem = useCallback((e) => {
+    console.log('delete');
+    dispatch({type: 'delete-item'});
+  }, []);
+
+  const toggleOpenModal = useCallback((e) => {
+    dispatch({type: 'open-add-item-modal'});
+  }, []);
+
+  const toggleModalOnSubmit = useCallback(() => {
+    dispatch({type: 'close-item-modal'});
+  }, []);
+
+  const toggleModalOnClose = useCallback(() => {
+    dispatch({type: 'close-item-modal'});
+  }, []);
+
+  return (
+    <Modal size="lg" show={isOpen} onHide={onClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Meal Plan</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicName">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter Name"
+              value={mealPlanName}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicDescription">
+            <Form.Label>Description</Form.Label>
+            <Form.Control
+              as="textarea"
+              placeholder="Description"
+              rows={3}
+              value={mealPlanDescription}
+            />
+          </Form.Group>
+          <Container>
+          <ItemModal isOpen={state.modalOpen} onClose={toggleModalOnClose} onSubmit={toggleModalOnSubmit}
+          mealPlanName={state.selectedMealPlanName} mealPlanDescription={state.selectedMealPlanDescription} />
+            <Button variant="success" onClick={toggleOpenModal}>Add new item</Button>
+            <Table striped bordered hover>
+              <tr>
+                <th>Name</th>
+                <th>Category</th>
+                <th>Actions</th>
+              </tr>
+              {items.map((item, index) => (
+                <tr>
+                  <td>{item.name}</td>
+                  <td>This is food</td>
+                  <td>
+                    <div className="mb-2">
+                      <Button id={index} variant="danger" size="sm" onClick={toggleDeleteItem}>
+                        Delete
+                      </Button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </Table>
+          </Container>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={onClose}>
+          Close
+        </Button>
+        <Button variant="primary" onClick={onSubmit}>
+          Save Changes
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+const Container = styled.div`
+  margin: 10px 10px 10px 10px;
+  th,
+  tr {
+    text-align: center;
+  }
+`;
+
+export default MealPlanModal;
