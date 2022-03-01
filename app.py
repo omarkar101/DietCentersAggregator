c0450cde9bec2b966b608915afb433d4ac7bbf9f
Rony_Allaw <rga45@mail.aub.edu>
2022-03-01 19:25:26 +0200

    added require user

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from auth.api import auth_api
-from auth.decorators import token_required
+from auth.decorators import require_user
 
 from database.models.credentials import Credentials
 from database.models.addresses import Address
@@ -12,13 +12,13 @@ from database.models.service_providers_meal_plans import ServiceProviderMealPlan
 from database.models.meal_plans_prices import MealPlanPrice
 from database.models.items import Item
 from database.models.categories import Category
+from user import UserType
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'this_key_here'
 app.register_blueprint(auth_api)
-
 @app.route('/',methods = ['POST'])
-@token_required
+@require_user(UserType.CLIENT)
 def hello():
     return 'hello'
 if __name__ == '__main__':
