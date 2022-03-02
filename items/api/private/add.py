f848e5cab568437bcaf75d5f8eb09bab1825230c
omarkar101 <omar.kar101@gmail.com>
2022-03-02 06:38:57 +0200

    added some apis

--- items/api/private/add.py
+++ items/api/private/add.py
@@ -1,3 +1,13 @@
-from flask import Blueprint
-
+from flask import Blueprint, jsonify
+from auth.decorators import require_user
+from user import get_user
 add_api = Blueprint('add_api', __name__, url_prefix='/add')
+
+@add_api.route('/', methods=['POST'])
+
+@require_user
+def get_all_items():
+  # we need to know which user is logged in
+  user = get_user()
+  items = user.service_provider.items
+  return jsonify(success=True, items=items)
--- items/api/private/get.py
+++ items/api/private/get.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify
 
 from auth.decorators import require_user
 from database.orm import generate_db_session
-from user import get_user
+from user import get_user, get_user_email_from_token
 
 get_api = Blueprint('get_api', __name__, url_prefix='/get')
 
@@ -11,4 +11,5 @@ get_api = Blueprint('get_api', __name__, url_prefix='/get')
 def get_all_items():
   # we need to know which user is logged in
   user = get_user()
+  items = user.service_provider.items
   return jsonify(success=True, items=items)
