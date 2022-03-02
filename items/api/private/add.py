678fef9f6b01ac6c88c155a289645b29aa7614ad
omarkar101 <omar.kar101@gmail.com>
2022-03-02 07:44:17 +0200

    edited apis

--- items/api/private/add.py
+++ items/api/private/add.py
@@ -1,13 +1,23 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from auth.decorators import require_user
-from user import get_user
+from database.models.items import Item
+from database.orm import generate_db_session
+from user import UserType, get_user
+
 add_api = Blueprint('add_api', __name__, url_prefix='/add')
 
 @add_api.route('/', methods=['POST'])
-
-@require_user
-def get_all_items():
+@require_user(UserType.SERVICE_PROVIDER)
+def add_item():
   # we need to know which user is logged in
-  user = get_user()
-  items = user.service_provider.items
+  # user = get_user()
+  # items = user.service_provider.items
+  item_name = request.get_json().get('item_name')
+  item_description = request.get_json().get('item_description')
+  with generate_db_session() as db_session:
+    # we should associate the items to the current user
+    item = Item(name=item_name, description=item_description)
+    db_session.add(item)
+    # we should only get the current user's items
+    items = db_session.query(Item).all()
   return jsonify(success=True, items=items)
--- items/api/private/get.py
+++ items/api/private/get.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
-
 from auth.decorators import require_user
+from database.models.items import Item
 from database.orm import generate_db_session
 from user import get_user, get_user_email_from_token
 
@@ -10,6 +10,8 @@ get_api = Blueprint('get_api', __name__, url_prefix='/get')
 @require_user
 def get_all_items():
   # we need to know which user is logged in
-  user = get_user()
-  items = user.service_provider.items
+  # user = get_user()
+  # items = user.service_provider.items
+  with generate_db_session() as db_session:
+    items = db_session.query(Item).all()
   return jsonify(success=True, items=items)
