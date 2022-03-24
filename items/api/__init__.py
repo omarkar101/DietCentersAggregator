fc2819d79228c83559a6e067a6caf060e4fc5e11
Ali <73133529+SrourHub@users.noreply.github.com>
2022-03-24 19:36:15 +0200

    check it now omar pls

--- items/api/__init__.py
+++ items/api/__init__.py
@@ -7,9 +7,7 @@ from items.api.private.edit import edit_api
 
 items_api = Blueprint('items_api', __name__, url_prefix='/items')
 items_api.register_blueprint(get_api)
+items_api.register_blueprint(public_get_api)
 items_api.register_blueprint(add_api)
 items_api.register_blueprint(delete_api)
 items_api.register_blueprint(edit_api)
-
-public_items_api = Blueprint('public_items_api', __name__, url_prefix='/items')
-public_items_api.register_blueprint(public_get_api)
--- meal_plans/api/__init__.py
+++ meal_plans/api/__init__.py
@@ -7,9 +7,7 @@ from meal_plans.api.public.get import public_get_api
 
 meal_plans_api = Blueprint('meal_plans_api', __name__, url_prefix='/meal_plans')
 meal_plans_api.register_blueprint(get_api)
+meal_plans_api.register_blueprint(public_get_api)
 meal_plans_api.register_blueprint(add_api)
 meal_plans_api.register_blueprint(delete_api)
 meal_plans_api.register_blueprint(edit_api)
-
-public_meal_plans_api = Blueprint('public_meal_plans_api', __name__, url_prefix='/meal_plans')
-public_meal_plans_api.register_blueprint(public_get_api)
