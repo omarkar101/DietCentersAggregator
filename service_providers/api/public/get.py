31063e045ee4fdce4b0aaa145056d97fb3f89a32
omarkar101 <omar.kar101@gmail.com>
2022-03-25 22:24:48 +0200

    .

--- service_providers/api/public/get.py
+++ service_providers/api/public/get.py
@@ -1,9 +1,6 @@
-from sre_constants import SUCCESS
 from flask import Blueprint, jsonify
-from auth.decorators import require_user
 from database.models.service_providers import ServiceProvider
 from database.orm import generate_db_session
-from user import get_user, get_user_email_from_token
 
 public_get_api = Blueprint('public_get_api', __name__, url_prefix='/public/get')
 
