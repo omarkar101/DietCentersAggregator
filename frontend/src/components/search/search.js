08f1b855edc4f8a27e440c909a903015dcdd75d6
Ali <73133529+SrourHub@users.noreply.github.com>
2022-03-27 21:25:41 +0300

    Update search.js

--- frontend/src/components/search/search.js
+++ frontend/src/components/search/search.js
@@ -5,46 +5,49 @@ import { searchForServiceProvidersByName } from "../../api/requests";
 import ExploreSection from "../common/exploreSection";
 
 const reducer = (state, action) => {
-    switch (action.type) {
-      case "search-service-providers-by-name":
-        return { ...state, serviceProviders: action.serviceProviders };
-      default:
-        throw new Error();
-    }
+  switch (action.type) {
+    case "search-service-providers-by-name":
+      return { ...state, serviceProviders: action.serviceProviders };
+    default:
+      throw new Error();
   }
+};
 
-  
 const Search = () => {
+  const location = useLocation();
+  const [state, dispatch] = useReducer(reducer, {
+    serviceProviders: [],
+  });
 
-    const location = useLocation();
-    const [state, dispatch] = useReducer(reducer, {
-        serviceProviders: [],
-    });
-    
-    useEffect(() => {
-        console.log(location.state.serviceProviderName);
-        if (location.state.serviceProviderName == null) {
-            return;
+  useEffect(() => {
+    if (location.state.serviceProviderName == null) {
+      return;
+    }
+    searchForServiceProvidersByName(location.state.serviceProviderName)
+      .then((response) => {
+        if (response.data.success) {
+          console.log(response.data.service_providers);
+          dispatch({
+            type: "search-service-providers-by-name",
+            serviceProviders: response.data.service_providers,
+          });
+        } else {
+          alert(response.data.message);
         }
-        searchForServiceProvidersByName(location.state.serviceProviderName)
-        .then((response) => {
-            if (response.data.success) {
-                console.log(response.data.service_providers);
-                dispatch({ type: 'search-service-providers-by-name', serviceProviders: response.data.service_providers })
-            } else {
-            alert(response.data.message);
-            }
-        })
-        .catch((e) => {
-            alert(e);
-        })
-      }, [location.state.serviceProviderName]);
+      })
+      .catch((e) => {
+        alert(e);
+      });
+  }, [location.state.serviceProviderName]);
 
-    return (
-        <>
-          <ExploreSection list={state.serviceProviders == null ? [] : state.serviceProviders } collectionName={`${state.serviceProviders?.length} result(s) found`} />
-        </>
-    );
-}
+  return (
+    <>
+      <ExploreSection
+        list={state.serviceProviders == null ? [] : state.serviceProviders}
+        collectionName={`${state.serviceProviders?.length} result(s) found`}
+      />
+    </>
+  );
+};
 
 export default Search;
