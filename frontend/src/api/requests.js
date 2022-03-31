53971a24245cadc0093d876d9346afc85d3ccf2c
Rony_Allaw <rga45@mail.aub.edu>
2022-03-31 04:02:13 +0300

    made user save profile

--- frontend/src/api/requests.js
+++ frontend/src/api/requests.js
@@ -28,3 +28,27 @@ export const searchForServiceProvidersByName = (serviceProviderName) => {
   data.append('service_provider_name', serviceProviderName);
   return Post('/service_providers/public/get/search', data);
 }
+
+export const updateClientProfile = (firstName, LastName, PhoneNumber, Weight, Height, Age,
+                                    addressEmail, addressName, addressPhoneNumber, city, country,
+                                    street, building, floor, receiverFirstName, receiverLastName, instructions) => {
+  const data = new FormData();
+  data.append('first_name', firstName);
+  data.append('last_name', LastName);
+  data.append('phone_number', PhoneNumber);
+  data.append('weight', Weight);
+  data.append('height', Height);
+  data.append('age', Age);
+  data.append('address_email', addressEmail);
+  data.append('address_name', addressName);
+  data.append('address_phone_number', addressPhoneNumber);
+  data.append('country', country);
+  data.append('city', city);
+  data.append('street', street);
+  data.append('building', building);
+  data.append('floor', floor);
+  data.append('address_first_name', receiverFirstName);
+  data.append('address_last_name', receiverLastName);
+  data.append('instructions', instructions);
+  return Post('/user/public/update/personal_info', data);
+}
