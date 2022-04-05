6bc2e8fef7611393e2c3c2b93b7e5d536284ac53
Ali <73133529+SrourHub@users.noreply.github.com>
2022-04-05 15:44:06 +0300

    Update get.py

--- service_providers/api/public/get.py
+++ service_providers/api/public/get.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify, request
+from sqlalchemy.orm import joinedload
 from database.models.service_providers import ServiceProvider
 from database.orm import generate_db_session
 
@@ -22,3 +23,16 @@ def get_service_providers_by_name():
   service_providers_copy = [service_provider for service_provider in service_providers if service_provider.name.startswith(serviceProviderName)]
 
   return jsonify(success=True, service_providers=service_providers_copy)
+
+@public_get_api.route('/by_id', methods=['POST'])
+def get_service_provider_by_id():
+  service_provider_id = request.form.get('service_provider_id')
+  with generate_db_session() as db_session:
+    service_provider = db_session.query(ServiceProvider) \
+      .filter(ServiceProvider.user_id == service_provider_id) \
+      .options(joinedload(ServiceProvider.user)) \
+      .first()
+
+  # print(service_provider.as_dict())
+  return jsonify(success=True, service_provider=service_provider)
+
