b8ba1a1fd1b6f2628a5c3479c1d75ef3ab716db7
Julia Kammouni <juliakammouni1@gmail.com>
2022-04-18 00:09:23 +0300

    subscription details

--- user/api/__init__.py
+++ user/api/__init__.py
@@ -1,9 +1,9 @@
 from flask import Blueprint
 from user.api.public.update_profile import public_update_api
 from user.api.private.add import add_api
-from user.api.private.get import private_get_api
+from user.api.public.get import public_get_api
 
 user_api = Blueprint('user_api', __name__, url_prefix='/user')
 user_api.register_blueprint(public_update_api)
 user_api.register_blueprint(add_api)
-user_api.register_blueprint(private_get_api)
+user_api.register_blueprint(public_get_api)
