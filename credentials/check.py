64b12e0bd9fa5dddc3e2f9aa818aa850a8fee47e
omarkar101 <omar.kar101@gmail.com>
2022-04-18 19:04:29 +0300

    fixed query

--- credentials/check.py
+++ credentials/check.py
@@ -1,5 +1,5 @@
 from database.models.credentials import Credentials
 
 def check_credentials(email, password):
-    credentials = Credentials.filter(Credentials.email == email).first()
+    credentials = Credentials.query.filter(Credentials.email == email).first()
     return credentials.compare_password(password) if credentials is not None else False
