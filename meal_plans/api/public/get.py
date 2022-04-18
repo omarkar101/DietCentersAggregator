988d3c040ec2eb63ebfb0eb2c8bc2632ed19d705
omarkar101 <omar.kar101@gmail.com>
2022-04-18 20:06:29 +0300

    removed list_to_dict_list

--- meal_plans/api/public/get.py
+++ meal_plans/api/public/get.py
@@ -4,19 +4,12 @@ from flask_cors import cross_origin
 from sqlalchemy.orm import joinedload
 from database.models.service_providers_meal_plans import ServiceProviderMealPlan
 from database.orm import db_session
-from utils import list_to_dict_list
 public_get_api = Blueprint('public_get_api', __name__, url_prefix='/public/get')
 
 @public_get_api.route('/all', methods=['GET'])
-# @require_user
 def get_all_meal_plans():
-  # we need to know which user is logged in
-  # user = get_user_id()
-  # items = user.service_provider.items
-  with db_session.begin():
-    meal_plans = db_session.query(ServiceProviderMealPlan).all()
-    target_list = list_to_dict_list(meal_plans)
-  return jsonify(success=True, meal_plans=target_list)
+  meal_plans = db_session.query(ServiceProviderMealPlan).all()
+  return jsonify(success=True, meal_plans=[meal_plan for meal_plan in meal_plans])
 
 @public_get_api.route('/by_id', methods=['POST'])
 @cross_origin(origins='*', supports_credentials=True)
@@ -32,12 +25,10 @@ def get_meal_plan_by_id():
 @public_get_api.route('/of_service_provider', methods=['POST'])
 def get_meal_plans_of_service_provider():
   service_provider_id = request.form.get('service_provider_id')
-  with db_session.begin():
-    meal_plans = db_session.query(ServiceProviderMealPlan) \
-      .filter(ServiceProviderMealPlan.user_id == service_provider_id) \
-      .all()
-    target_list = list_to_dict_list(meal_plans)
-  return jsonify(success=True, meal_plans=target_list)
+  meal_plans = db_session.query(ServiceProviderMealPlan) \
+    .filter(ServiceProviderMealPlan.user_id == service_provider_id) \
+    .all()
+  return jsonify(success=True, meal_plans=[meal_plan for meal_plan in meal_plans])
 
 @public_get_api.route('items_of_meal_plan_of_service_provider', methods=['POST'])
 @cross_origin(origins='*', supports_credentials=True)
--- utils.py
+++
@@ -1,5 +0,0 @@
-def list_to_dict_list(model_list):
-    target_list = []
-    for model in model_list:
-        target_list.append(model.as_dict())
-    return target_list
