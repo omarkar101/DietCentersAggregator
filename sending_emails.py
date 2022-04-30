45bd8f08f1e3159418dcb580f2e5f2b51dcad191
omarkar101 <omar.kar101@gmail.com>
2022-05-01 01:06:09 +0300

    fixed email sender

--- sending_emails.py
+++ sending_emails.py
@@ -1,17 +1,27 @@
 import smtplib
-  
-def sendEmail(emailAddress, message):
-  senderEmail = "299jaro@gmail.com"
-  senderPassword = "Jarocmps299"
-  # creates SMTP session
-  s = smtplib.SMTP('smtp.gmail.com', 587)
-  # start TLS for security
-  s.starttls()
-  # Authentication
-  s.login(senderEmail,senderPassword)
-  # sending the mail
-  s.sendmail(senderEmail, emailAddress, message)
-  # terminating the session
-  s.quit()
 
-# sendEmail('yamout.samer101@gmail.com', 'hellooooo')
+def sendEmail(emailAddress, message, subject=''):
+  gmail_user = '299jaro@gmail.com'
+  gmail_password = 'Jarocmps299'
+
+  sent_from = gmail_user
+  to = [emailAddress]
+  body = message
+
+  email_text = """\
+  From: %s
+  To: %s
+  Subject: %s
+
+  %s
+  """ % (sent_from, ", ".join(to), subject, body)
+
+  try:
+    smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    smtp_server.ehlo()
+    smtp_server.login(gmail_user, gmail_password)
+    smtp_server.sendmail(sent_from, to, email_text)
+    smtp_server.close()
+    print ("Email sent successfully!")
+  except Exception as ex:
+    print ("Something went wrong….",ex)
